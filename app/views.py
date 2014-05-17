8c86ea8c2702a78dae484f02f1a6f286fdf44cea
Cody Ray Hoeft <crhoeft@gmail.com>
2014-05-17 13:31:07 -0700

    Working Proof of Concept

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
-from app import app
+from app import app, db, models
+from flask import render_template, Response
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    return render_template("index.html",
+        title = "Welcome",
+        carname = "Pheonix",
+        orgname = "OSU Solar Vehicle Team")
+
+@app.route('/current/<data>/')
+def current_data(data):
+    if(data == 'soc'):
+        soc = models.body_control.query.order_by(models.body_control.time.desc()).first()
+        lsoc = [soc.soc]
+        return Response(json.dumps(lsoc), mimetype='application/json')
+
+@app.route('/gauge/<data>/')
+def gauge(data):
+    return render_template("chart.html")
