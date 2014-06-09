014af963e7ca189ffffd8b15245b9c3a46073607
Cody Ray Hoeft <crhoeft@gmail.com>
2014-06-08 17:06:39 -0700

    Changing how querys work, config is stored

--- app/views.py
+++ app/views.py
@@ -3,45 +3,13 @@ from flask import render_template, Response
 import json, time, operator
 from datetime import datetime
 
-ORGNAME = "OSU Solar Vehicle Team"
-CARNAME = "Pheonix"
-
-gaugesettings = dict({
-    "mainpacksoc": {
-        "title": "State of Charge (Main Pack)",
-        "minval": 0,
-        "maxval": 100,
-        "units": "units"
-    },
-    "elevation": {
-        "title": "Elevation",
-        "minval": 0,
-        "maxval": 1000,
-        "units": "units"
-    },
-    "velocity": {
-        "title": "Velocity",
-        "minval": 0,
-        "maxval": 100,
-        "units": "units"
-    },
-    "mainpackvoltage": {
-        "title": "Main Pack Voltage",
-        "minval": 0,
-        "maxval": 200,
-        "units": "units"
-    }
-})
-guageupdate = 5000
-
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html",
                            title="Welcome",
-                           carname=CARNAME,
-                           orgname=ORGNAME
+                           carname=app.config["CARNAME"],
+                           orgname=app.config["ORGNAME"]
                            )
 
 
@@ -50,7 +18,7 @@ def index():
 def current_data(data):
     query = models.telemetry.query.order_by(models.telemetry.identity.desc()).first()
     # This is bad codding find a different way...
-    exec ("value = [query." + data + " ]")
+    value = [query.__dict__.get(data)]
     return Response(json.dumps(value), mimetype='application/json')
 
 
@@ -59,7 +27,10 @@ def current_data(data):
 def all_data(data):
     query = models.telemetry.query.order_by(models.telemetry.identity.desc()).all()
     values = []
-    exec ("values = [(int(time.mktime(datetime.strptime(value.datetime, '%m/%d/%Y %H:%M:%S %p').timetuple())+1e-6*datetime.strptime(value.datetime, '%m/%d/%Y %H:%M:%S %p').microsecond) * 1000, value." + data + ") for value in query]")
+    for value in query:
+        values.append((int(time.mktime(
+            datetime.strptime(value.datetime, '%m/%d/%Y %H:%M:%S %p').timetuple()) + 1e-6 * datetime.strptime(
+            value.datetime, '%m/%d/%Y %H:%M:%S %p').microsecond) * 1000, value.__dict__.get(data)))
     #values.sort(reverse=True)
     values.reverse()
     return Response(json.dumps(values), mimetype='application/json')
@@ -70,7 +41,10 @@ def all_data(data):
 def prev_data(data, num):
     query = models.telemetry.query.order_by(models.telemetry.identity.desc()).limit(num)
     values = []
-    exec ("values = [(int(time.mktime(datetime.strptime(value.datetime, '%m/%d/%Y %H:%M:%S %p').timetuple())+1e-6*datetime.strptime(value.datetime, '%m/%d/%Y %H:%M:%S %p').microsecond) * 1000, value." + data + ") for value in query]")
+    for value in query:
+        values.append((int(time.mktime(
+            datetime.strptime(value.datetime, '%m/%d/%Y %H:%M:%S %p').timetuple()) + 1e-6 * datetime.strptime(
+            value.datetime, '%m/%d/%Y %H:%M:%S %p').microsecond) * 1000, value.__dict__.get(data)))
     #values.sort(reverse=True)
     values.reverse()
     return Response(json.dumps(values), mimetype='application/json')
@@ -80,15 +54,15 @@ def prev_data(data, num):
 @app.route('/<data>/gauge/')
 def gauge(data):
     return render_template("gauge.html",
-                           title="Gauge - {0}".format(gaugesettings[data]["title"]),
-                           carname=CARNAME,
-                           orgname=ORGNAME,
-                           minval=gaugesettings[data]["minval"],
-                           maxval=gaugesettings[data]["maxval"],
-                           charttitle=gaugesettings[data]["title"],
-                           suffix=gaugesettings[data]["units"],
+                           title="Gauge - {0}".format(app.config["ITEMPROP"][data]["title"]),
+                           carname=app.config["CARNAME"],
+                           orgname=app.config["ORGNAME"],
+                           minval=app.config["ITEMPROP"][data]["minval"],
+                           maxval=app.config["ITEMPROP"][data]["maxval"],
+                           charttitle=app.config["ITEMPROP"][data]["title"],
+                           suffix=app.config["ITEMPROP"][data]["units"],
                            data=data,
-                           update=guageupdate
+                           update=app.config["UPDATE"]
                            )
 
 
@@ -97,7 +71,7 @@ def gauge(data):
 def chart(data):
     return render_template("long.html",
                            title="Long Chart",
-                           carname=CARNAME,
-                           orgname=ORGNAME,
+                           carname=app.config.CARNAME,
+                           orgname=app.config.ORGNAME,
                            data=data,
                            )
--- config.py
+++ config.py
@@ -7,5 +7,40 @@ SQLDATABASE = 'Telemetry'
 
 
 #SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://" + SQLUSERNAME + ":" + SQLPASSWORD + "@localhost/" + SQLDATABASE + "?charset=utf8&use_unicode=0"
-SQLALCHEMY_DATABASE_URI = "sqlite:////"+basedir+"/app.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:////"+os.path.abspath("app.db")
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+ORGNAME = "OSU Solar Vehicle Team"
+CARNAME = "Pheonix"
+
+
+UPDATE = 5000
+
+
+ITEMPROP = dict({
+    "mainpacksoc": {
+        "title": "State of Charge (Main Pack)",
+        "minval": 0,
+        "maxval": 100,
+        "units": "units"
+    },
+    "elevation": {
+        "title": "Elevation",
+        "minval": 0,
+        "maxval": 1000,
+        "units": "units"
+    },
+    "velocity": {
+        "title": "Velocity",
+        "minval": 0,
+        "maxval": 100,
+        "units": "units"
+    },
+    "mainpackvoltage": {
+        "title": "Main Pack Voltage",
+        "minval": 0,
+        "maxval": 200,
+        "units": "units"
+    }
+})
