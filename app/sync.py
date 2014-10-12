9ccde3d309ca049e09c610f7aadc8a0dab443152
Cody Ray Hoeft <hoeftc@onid.oregonstate.edu>
2014-10-12 18:21:34 +0000

    Added Compatibility for sqlite memory database

--- app/sync.py
+++ app/sync.py
@@ -1,21 +1,21 @@
 import app
-from app.database import telemetry #We need the table definition
+from app.database import telemetry, engine #We need the table definition
 import sqlalchemy
-import time
 import multiprocessing
 
 sourcedb = sqlalchemy.create_engine(app.app.config["SOURCEDB"], echo=False);
-telemetrydb = sqlalchemy.create_engine(app.app.config["TELEMETRYDB"], echo=False);
 telemetrymaxid = 0
-sourcemaxid = 0
 
-debug = True
+#When debug is True, instead of trying to syncronize the databases as fast as possible it increments them in steps equal to debuglimit, this can be usefull for testing how quickly the interface updates
+debug = False
+debuglimit = 100
 
 #app.database.metadata.create_all(sourcedb) #Should not be needed because the database already exists, but it should produce an error if the definition doesn't match
 
 
 
 def daemon():
+    sync = sync_select()
     #Code for running first sync
     print "Starting First Sync, if there is alot of data this could take a while"
     sync()
@@ -27,28 +27,47 @@ def daemon():
 
 
 def loop():
+    sync = sync_select()
     while(1):
-	sync()
+	try: #The Show Must go on!
+	    sync()
+	except sqlalchemy.exc.DBAPIError:
+	    pass #Raised when execution of database operations fails
+	except sqlalchemy.exc.SQLAlchemyError:
+	    pass #General SQLAlchemy error class
 
 
-def sync():
-    telemetrymaxid = maxid(telemetrydb)
-    sourcemaxid = maxid(sourcedb)
-    if countrows(sourcedb) or sourcemaxid > telemetrymaxid:
+def sync_select():
+	if engine.driver == "mysqldb" and sourcedb.driver == "mysqldb":
+		return sync_mysql 
+		# sync_mysql operates under the assumption that the databases are both mysql. It uses a literal sql expression to add the data that is very fast
+	else:
+		return sync_sqlalchemy
+		# sync_sqlalchemy will take advantage of sqlalchemy's abstractions to work on most compatable databases
+
+@app.timeit
+def sync_mysql():
+	telemetrymaxid = maxid(engine)
 	#Query for values greater than the ones that we have
 	if debug:
-		s = sqlalchemy.sql.expression.text("INSERT INTO telemetry.telemetry SELECT * FROM telemetry WHERE telemetry.id > :telemetrymaxid ORDER BY telemetry.id LIMIT :limit").bindparams(telemetrymaxid=telemetrymaxid, limit=1)
+		s = sqlalchemy.sql.expression.text("INSERT INTO telemetry.telemetry SELECT * FROM telemetry WHERE telemetry.id > :telemetrymaxid ORDER BY telemetry.id LIMIT :limit").bindparams(telemetrymaxid=telemetrymaxid, limit=debuglimit)
 	else:
 		s = sqlalchemy.sql.expression.text("INSERT INTO telemetry.telemetry SELECT * FROM telemetry WHERE telemetry.id > :telemetrymaxid ORDER BY telemetry.id").bindparams(telemetrymaxid=telemetrymaxid)
 	sourcedb.execute(s) #get data from source
 
 
-def countrows(engine):
-	s = sqlalchemy.sql.select([sqlalchemy.func.count(telemetry.c.id)])
-	result = engine.execute(s).fetchall()[0][0]
-	if result is None:
-		result = 0
-	return result
+@app.timeit
+def sync_sqlalchemy():
+	telemetrymaxid = maxid(engine)
+	if debug:
+		s = sqlalchemy.sql.select([telemetry]).where(telemetry.c.id > telemetrymaxid).limit(debuglimit)
+	else:
+		s = sqlalchemy.sql.select([telemetry]).where(telemetry.c.id > telemetrymaxid)
+	result = sourcedb.execute(s).fetchall()
+	#Error will be produced here if result contains to data, but it will get handled in the try except in loop()
+	s = sqlalchemy.sql.insert(telemetry)
+	engine.execute(s, result)
+	
 
 
 def maxid(engine):
