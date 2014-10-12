1f14b38742cd552fa648fb2026db9c6d5d72b14f
Cody Ray Hoeft <hoeftc@onid.oregonstate.edu>
2014-10-12 20:17:41 +0000

    Sharing sqlite memory accross processes not possible, file must be used

--- app/sync.py
+++ app/sync.py
@@ -8,22 +8,19 @@ telemetrydb = sqlalchemy.create_engine(app.app.config["TELEMETRYDB"], echo=False
 telemetrymaxid = 0
 
 #When debug is True, instead of trying to syncronize the databases as fast as possible it increments them in steps equal to debuglimit, this can be usefull for testing how quickly the interface updates
-debug = False
-debuglimit = 100
+debug = True
+debuglimit = 1
 
 #app.database.metadata.create_all(sourcedb) #Should not be needed because the database already exists, but it should produce an error if the definition doesn't match
 
 
-l = multiprocessing.Lock()
-
-
 def daemon():
     #What sync method are we using?
-    sync, engine = sync_select()
+    sync = sync_select()
     print sync
     #Code for running first sync
     print "Starting First Sync, if there is alot of data this could take a while"
-    sync(engine)
+    sync()
     print "First Sync Complete, Launching Daemon"
     #Code for launching further syncs in a separate proccess here
     daemon_process = multiprocessing.Process(target=loop, name="syncloop")
@@ -31,11 +28,11 @@ def daemon():
     daemon_process.start()
 
 
-def loop(once=False):
-    sync, engine = sync_select()
+def loop():
+    sync = sync_select()
     while(1):
 	try: #The Show Must go on!
-	    sync(engine)
+	    sync()
 	except sqlalchemy.exc.DBAPIError:
 	    pass #Raised when execution of database operations fails
 	except sqlalchemy.exc.SQLAlchemyError:
@@ -44,14 +41,14 @@ def loop(once=False):
 
 def sync_select():
 	if telemetrydb.driver == "mysqldb" and sourcedb.driver == "mysqldb":
-		return sync_mysql, telemetrydb
+		return sync_mysql
 		# sync_mysql operates under the assumption that the databases are both mysql. It uses a literal sql expression to add the data that is very fast
 	else:
-		return sync_sqlalchemy, app.database.engine
+		return sync_sqlalchemy
 		# sync_sqlalchemy will take advantage of sqlalchemy's abstractions to work on most compatable databases
 
-def sync_mysql(engine):
-	telemetrymaxid = maxid(engine)
+def sync_mysql():
+	telemetrymaxid = maxid(telemetrydb)
 	#Query for values greater than the ones that we have
 	if debug:
 		s = sqlalchemy.sql.expression.text("INSERT INTO telemetry.telemetry SELECT * FROM telemetry WHERE telemetry.id > :telemetrymaxid ORDER BY telemetry.id LIMIT :limit").bindparams(telemetrymaxid=telemetrymaxid, limit=debuglimit)
@@ -60,10 +57,8 @@ def sync_mysql(engine):
 	sourcedb.execute(s)
 
 
-def sync_sqlalchemy(engine):
-	l.acquire()
-	telemetrymaxid = maxid(engine)
-	l.release()
+def sync_sqlalchemy():
+	telemetrymaxid = maxid(telemetrydb)
 	if debug:
 		s = sqlalchemy.sql.select([telemetry]).where(telemetry.c.id > telemetrymaxid).limit(debuglimit)
 	else:
@@ -71,9 +66,7 @@ def sync_sqlalchemy(engine):
 	result = sourcedb.execute(s).fetchall()
 	#Error will be produced here if result contains to data, but it will get handled in the try except in loop()
 	s = sqlalchemy.sql.insert(telemetry)
-	l.acquire()
-	engine.execute(s, result)
-	l.release()
+	telemetrydb.execute(s, result)
 	
 
 
--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 SOURCEDB = 'mysql+mysqldb://solar:Phenix@localhost/solarcar'
 
 #This is the Database used by Telemetry to display data, there are two basic options
-TELEMETRYDB = 'sqlite://'
+TELEMETRYDB = 'sqlite:///temp.db'
 	#Use database that only exists in memory. Use this if you want an easy setup
 		#Pros: Don't require mysql on the laptop/box running Telemetry, starts fresh when code starts again
 		#Cons: Slow to start up, no persistant data, sqlite doesn't support 'decimal' type so rounding errors will occur as the data gets converted to float
